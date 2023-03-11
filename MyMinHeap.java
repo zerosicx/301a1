ef382101903f9b1dec0082ee56c912f01def5c74
Hannah Carino <hcarino.official@gmail.com>
2023-03-11 19:46:24 +1300

    Created necessary files for assignment and wrote skeleton for MyMinHeap.java

---
+++ MyMinHeap.java
@@ -0,0 +1,17 @@
+class MyMinHeap {
+    public static void main(str[] args){
+        System.out.println("Hello World");
+    }
+
+    public void insert(){}
+
+    public void remove(){}
+
+    public void replace(){}
+
+    public String preak(){return "";}
+
+    public void load(){}
+
+    public void reheap(){}
+}
