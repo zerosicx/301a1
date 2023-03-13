3a8e26bd00f7608383f8bb833dbb6125f40cdea7
Kyle Ananayo <intanapananayo@gmail.com>
2023-03-14 11:21:22 +1300

    implemented methods replace(), peek(), load(), reheap() and helper function swap()

--- MyMinHeap.java
+++ MyMinHeap.java
@@ -34,18 +34,81 @@ class MyMinHeap {
         // TODO: add downheap to put root in the right place
     }
 
-    public void replace() {
-
+    
+    // replaces the first value in the heap and reheaps
+    public String replace(String s) {
+        // check if the first value of the heap array is 
+        if(heapArray.get(1)!=null){
+            // temp store first value in heap array
+            String out = heapArray.get(1);
+            // set the first value of the array to the given string
+            heapArray.set(1, s);
+            // rearrange the heap if necessary
+            reheap();
+            // return the smallest value of the array
+            return out;
+        }
+        // if heap array is empty, add the string to the top of array
+        heapArray.add(1, s);
+        return null;
     }
 
+
+    // returs the first value in the heap
     public String peek() {
-        return "";
+        // check if heap array is empty
+        if(heapArray.size() < 3){ return null;}
+        // return the first value in hte heap
+        return heapArray.get(1);
     }
 
-    public void load() {
+    // load the given array as heap
+    public void load(ArrayList<String> stringArray) {
+        // add contents of the string array to the heap array
+        for(String s: stringArray){
+            heapArray.add(s);
+        }
     }
 
+    // rearranges the heap so the first value has the least value
     public void reheap() {
+
+        int indexParent = 1;
+        String parent = heapArray.get(indexParent);
+
+
+        // check if parent is greater any of its children
+        while(parent.compareTo(heapArray.get(2*indexParent))>0 || 
+            parent.compareTo(heapArray.get(2*indexParent+1))>0){
+
+            int indexChildL = indexParent*2;
+            int indexChildR = (indexParent*2)+1;
+
+            // check if parent is greater than left child and swap
+            if(parent.compareTo(heapArray.get(2*indexParent))>0){
+                swap(indexParent,indexChildL);
+                parent = heapArray.get(indexChildL);
+                indexParent = heapArray.indexOf(parent);
+            }
+
+            // check if parent is greater than right child and swap
+            if(parent.compareTo(heapArray.get(2*indexParent+1))>0){
+                swap(indexParent,indexChildR);
+                parent = heapArray.get(indexChildR);
+                indexParent = heapArray.indexOf(parent);
+            }
+
+        }
+
+        return;
+    }
+
+
+    // swaps the values in the heap array with the given indexes
+    public void swap(int pos1, int pos2){
+        String string1 = heapArray.get(pos1);
+        heapArray.set(pos1, heapArray.get(pos2));
+        heapArray.set(pos2, string1);
     }
 
     public void printHeap() {
